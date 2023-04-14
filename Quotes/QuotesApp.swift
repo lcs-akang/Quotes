13a6c0c4f5931199754ab091b8fc7236e099f196
Aidan Kang <akang@lcs.on.ca>
2023-04-14 11:43:21 -0400

    Initial Commit

---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Aidan Kang on 2023-04-14.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
