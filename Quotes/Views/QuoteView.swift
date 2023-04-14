c5f8a64e56fa05beff9c43e4fcb2b6896f0b8b52
Aidan Kang <akang@lcs.on.ca>
2023-04-14 11:50:42 -0400

    Added quote and nav title.

--- Quotes/Views/QuoteView.swift
+++ Quotes/Views/QuoteView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct QuoteView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Text("True silence is the rest of the mind; it is to the spirit what sleep is to the body, nourishment and refreshment.")
+                    .font(.title2)
+                    .multilineTextAlignment(.center)
+            }
+            .navigationTitle("Random Quotes")
+        }
     }
 }
 
