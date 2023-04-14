a355b867190cb2c7f4199e7219ea6d4cf5852aaf
Aidan Kang <akang@lcs.on.ca>
2023-04-14 11:56:32 -0400

    Added a model to match the structure of the JSON object received from our endpoint.

---
+++ Quotes/Model/Quote.swift
@@ -0,0 +1,18 @@
+//
+//  Quote.swift
+//  Quotes
+//
+//  Created by Aidan Kang on 2023-04-14.
+//
+
+import Foundation
+
+struct Quote {
+    let text: String
+    let author: String
+    let link: String
+}
+
+let exampleQuote = Quote(text: "Sometimes by losing a battle you find a new way to win the war.",
+                         author: "Donald Trump",
+                         link: "http://forismatic.com/en/19307ae79b/")
